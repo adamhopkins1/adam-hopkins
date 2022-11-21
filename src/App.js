9cb233a13972e2f9a5cd7d91a0e0100a1fc30ee7
Adam Hopkins <Samsung3382@outlook.com>
2022-11-20 21:58:11 -0500

    Added and imported preview.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
 import News from './components/News';
+import Preview from './components/Preview';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Carousel />
       <News />
+      <Preview />
     </div>
     </>
   );
