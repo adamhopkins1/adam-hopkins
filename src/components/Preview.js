47f15c0c4eae26e526c9d5d1a09dc0d4690c690b
Adam Hopkins <Samsung3382@outlook.com>
2022-11-22 11:38:51 -0500

    commited preview.js

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,8 +3,8 @@ import React from "react";
 function Preview() {
   return (
     <>
-      <div className="flex flex-col mt-8">
-        <div className="container max-w-7xl px-4">
+      <div className="flex flex-wrap">
+        <div div className="container max-w-7xl px-4">
           <div className="flex flex-wrap justify-center text-center mb-24">
             <div className="w-full lg:w-6/12 px-4">
               <h1 className="text-gray-900 text-4xl font-bold mb-8">
@@ -21,8 +21,9 @@ function Preview() {
               </p>
             </div>
           </div>
+          </div>
+          
 
-          <div className="flex flex-wrap">
             <div className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
               <div className="flex flex-col">
                 <a href="#" className="mx-auto">
@@ -240,8 +241,7 @@ function Preview() {
               </div>
             </div>
           </div>
-        </div>
-      </div>
+
 
       <div className="flex flex-wrap">
         <div className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
